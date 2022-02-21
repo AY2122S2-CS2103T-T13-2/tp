a46b33c99ae9506cf61a92cfc620262f04950798
wx <liong@live.com.sg>
2022-02-21 19:13:53 +0800

    Fix Checkstyle violations

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates a Remark object.
+     *
+     * @param remark A remark message.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
